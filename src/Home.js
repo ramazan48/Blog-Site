339553e1a9294357a8de081bc80376fc041fe1f1
ramazan48 <77232741+ramazan48@users.noreply.github.com>
2024-08-02 16:27:46 +0300

    edited

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
   
     const handleDelete = async (id) => {
         try {
-          const response = await fetch(`https://grand-unicorn-c0189b.netlify.app/.netlify/functions/deletePost?id=${id}`, {
+          const response = await fetch(`/api/deletePost?id=${id}`, {
             method: 'DELETE',
           });
       
