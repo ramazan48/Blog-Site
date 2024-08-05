a06520d190fc2426488aa7d82672b6e506d50662
ramazan48 <77232741+ramazan48@users.noreply.github.com>
2024-08-05 14:14:31 +0300

    edited

--- public/server.js
+++ public/server.js
@@ -1,66 +1,39 @@
-const fs = require('fs');
 const express = require('express');
-const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-const PORT = 3000; 
 
-app.use(cors());
 app.use(express.json());
 
-const filePath = './blogposts.json';
+// JSON dosyasının yolu
+const jsonFilePath = path.join(__dirname, 'BlogPosts.json');
 
-app.get('/posts', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'BlogPosts.json'));
-});
+// Veriyi ekleme endpoint'i
+app.post('/api/addBlog', (req, res) => {
+  const newBlog = req.body;
 
-app.get('/posts', (req, res) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  // JSON dosyasını oku
+  fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Error reading data');
-    } else {
-      res.send(JSON.parse(data));
+      return res.status(500).json({ error: 'Failed to read JSON file' });
     }
+
+    // JSON verisini çöz
+    const blogs = JSON.parse(data);
+    blogs.push(newBlog);
+
+    // JSON dosyasını güncelle
+    fs.writeFile(jsonFilePath, JSON.stringify(blogs, null, 2), (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Failed to write JSON file' });
+      }
+
+      res.status(200).json({ message: 'Blog added successfully' });
+    });
   });
 });
 
-app.post('/posts', (req, res) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading data');
-    } else {
-      const posts = JSON.parse(data);
-      const newPost = req.body;
-      posts.push(newPost);
-      fs.writeFile(filePath, JSON.stringify(posts, null, 2), (err) => {
-        if (err) {
-          res.status(500).send('Error writing data');
-        } else {
-          res.status(201).send('Post added');
-        }
-      });
-    }
-  });
-});
-
-app.delete('/posts/:id', (req, res) => {
-  const { id } = req.params;
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading data');
-    } else {
-      let posts = JSON.parse(data);
-      posts = posts.filter(post => post.id !== id);
-      fs.writeFile(filePath, JSON.stringify(posts, null, 2), (err) => {
-        if (err) {
-          res.status(500).send('Error writing data');
-        } else {
-          res.send('Post deleted');
-        }
-      });
-    }
-  });
-});
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
